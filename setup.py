3ffdb80f898c4c42911385a81354cadeb19d3181
Alex Grönholm <alex.gronholm@nextday.fi>
2017-08-14 21:40:50 +0300

    Don't test with backports.typing on Python < 3.6
    Tests occasionally fail on 3.5 and lower when backports.typing is
    involved.
    
    Also dropped Python 3.3 from the testing matrix, as it will soon reach
    its end of life.

--- setup.py
+++ setup.py
@@ -23,7 +23,6 @@ setup(
         'Topic :: Documentation :: Sphinx',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6'
