7b13b6c8a8ea343a45c19575e5eda5066760f142
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2018-01-16 16:23:49 -0500

    Remove dummy project from temp on each test run. (#29)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
 import os
 import pytest
+import pathlib
+import shutil
 from sphinx.testing.path import path
 
 
@@ -8,7 +10,19 @@ pytest_plugins = 'sphinx.testing.fixtures'
 collect_ignore = ['roots']
 
 
+@pytest.fixture(scope='session')
+def remove_sphinx_projects(sphinx_test_tempdir):
+    # Remove any directory which appears to be a Sphinx project from
+    # the temporary directory area.
+    # See https://github.com/sphinx-doc/sphinx/issues/4040
+    roots_path = pathlib.Path(sphinx_test_tempdir)
+    for entry in roots_path.iterdir():
+        if entry.is_dir() and pathlib.Path(entry, '_build').exists():
+            shutil.rmtree(str(entry))
+    yield
+
+
 @pytest.fixture()
-def rootdir():
+def rootdir(remove_sphinx_projects):
     roots = path(os.path.dirname(__file__) or '.').abspath() / 'roots'
     yield roots
