87704ebaccb737094a9937b9ad77e88e83c17977
Evan Sangaline <evan@intoli.com>
2019-08-06 12:06:51 -0700

    Catch recursion errors when getting type hints (#92)
    Fixes #91.

--- sphinx_autodoc_typehints.py
+++ sphinx_autodoc_typehints.py
@@ -191,8 +191,9 @@ def get_all_type_hints(obj, name):
 
     try:
         rv = get_type_hints(obj)
-    except (AttributeError, TypeError):
-        # Introspecting a slot wrapper will raise TypeError
+    except (AttributeError, TypeError, RecursionError):
+        # Introspecting a slot wrapper will raise TypeError, and and some recursive type
+        # definitions will cause a RecursionError (https://github.com/python/typing/issues/574).
         pass
     except NameError as exc:
         logger.warning('Cannot resolve forward reference in type annotations of "%s": %s',
