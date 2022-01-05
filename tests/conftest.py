8115dedcb3013bc06dc6dbddaab1b64c6eb6edae
Bernát Gábor <bgabor8@bloomberg.net>
2022-01-05 11:52:07 +0000

    Add type information and check via mypy
    Signed-off-by: Bernát Gábor <bgabor8@bloomberg.net>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,14 @@
 from __future__ import annotations
 
 import os
-import pathlib
 import re
 import shutil
 import sys
+from pathlib import Path
+from typing import Any
 
 import pytest
+from _pytest.config import Config
 from sphinx.testing.path import path
 from sphobjinv import Inventory
 
@@ -15,8 +17,9 @@ collect_ignore = ["roots"]
 
 
 @pytest.fixture(scope="session")
-def inv(pytestconfig):
-    cache_path = "python{v.major}.{v.minor}/objects.inv".format(v=sys.version_info)
+def inv(pytestconfig: Config) -> Inventory:
+    cache_path = f"python{sys.version_info.major}.{sys.version_info.minor}/objects.inv"
+    assert pytestconfig.cache is not None
     inv_dict = pytestconfig.cache.get(cache_path, None)
     if inv_dict is not None:
         return Inventory(inv_dict)
@@ -29,28 +32,29 @@ def inv(pytestconfig):
 
 
 @pytest.fixture(autouse=True)
-def _remove_sphinx_projects(sphinx_test_tempdir):
+def _remove_sphinx_projects(sphinx_test_tempdir: path) -> None:
     # Remove any directory which appears to be a Sphinx project from
     # the temporary directory area.
     # See https://github.com/sphinx-doc/sphinx/issues/4040
-    roots_path = pathlib.Path(sphinx_test_tempdir)
+    roots_path = Path(sphinx_test_tempdir)
     for entry in roots_path.iterdir():
         try:
-            if entry.is_dir() and pathlib.Path(entry, "_build").exists():
+            if entry.is_dir() and Path(entry, "_build").exists():
                 shutil.rmtree(str(entry))
         except PermissionError:
             pass
 
 
 @pytest.fixture()
-def rootdir():
+def rootdir() -> path:
     return path(os.path.dirname(__file__) or ".").abspath() / "roots"
 
 
-def pytest_ignore_collect(path, config):  # noqa: U100
+def pytest_ignore_collect(path: Any, config: Config) -> bool | None:  # noqa: U100
     version_re = re.compile(r"_py(\d)(\d)\.py$")
     match = version_re.search(path.basename)
     if match:
         version = tuple(int(x) for x in match.groups())
         if sys.version_info < version:
             return True
+    return None
