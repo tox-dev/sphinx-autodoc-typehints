3dc2306fc76c5cdd7cdbb4be72d5d8954722d3f6
Simon Heybrock <simon.heybrock@ess.eu>
2022-02-11 12:36:26 +0100

    Add unit test

---
+++ tests/roots/test-dummy/dummy_module_simple_no_use_rtype.py
@@ -0,0 +1,25 @@
+def function_no_returns(x: bool, y: int = 1) -> str:  # noqa: U100
+    """
+    Function docstring.
+
+    :param x: foo
+    :param y: bar
+    """
+
+def function_returns_with_type(x: bool, y: int = 1) -> str:  # noqa: U100
+    """
+    Function docstring.
+
+    :param x: foo
+    :param y: bar
+    :returns: *CustomType* -- A string
+    """
+
+def function_returns_without_type(x: bool, y: int = 1) -> str:  # noqa: U100
+    """
+    Function docstring.
+
+    :param x: foo
+    :param y: bar
+    :returns: A string
+    """
---
+++ tests/roots/test-dummy/simple_no_use_rtype.rst
@@ -0,0 +1,6 @@
+Simple Module
+=============
+
+.. autofunction:: dummy_module_simple_no_use_rtype.function_no_returns
+.. autofunction:: dummy_module_simple_no_use_rtype.function_returns_with_type
+.. autofunction:: dummy_module_simple_no_use_rtype.function_returns_without_type
