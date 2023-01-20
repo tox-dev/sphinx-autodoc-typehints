fb30e405d37f94ecfc227caa9d8f397d526acad6
Hood Chatham <roberthoodchatham@gmail.com>
2023-01-19 20:01:04 -0800

    Refactor tests to make it easier to add new example functions (#306)
    * Refactor tests to make it easier to add more example functions
    
    This makes it so that it is possible to add a new example while only
    modifying one location in the code. Add a new class / function / exception
    in test_integration, indicate the expected output with the @expected decorator.
    More efficient than having to modify three different files like we had to do
    before.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Some minor improvements
    
    * Fix test
    
    * Ignore types in test_integration.py
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ tests/roots/test-integration/conf.py
@@ -0,0 +1,14 @@
+import pathlib
+import sys
+
+# Make dummy_module.py available for autodoc.
+sys.path.insert(0, str(pathlib.Path(__file__).parent))
+
+
+master_doc = "index"
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
+]
