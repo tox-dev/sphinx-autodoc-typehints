3eeb6641ae84e679636882269e03ed75dcc5a221
Felipe Peter <mr-peipei@web.de>
2023-11-01 01:28:23 +0900

    fix: Recursively evaluate guarded code (#393)
    * Add test for nested guarded imports
    
    * Handle nested type guards
    
    * Refactor
    
    ---------
    
    Co-authored-by: Bernát Gábor <gaborjbernat@gmail.com>

--- tests/roots/test-resolve-typing-guard/demo_typing_guard_dummy.py
+++ tests/roots/test-resolve-typing-guard/demo_typing_guard_dummy.py
@@ -1,6 +1,12 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
 from viktor import AI  # module part of autodoc_mock_imports # noqa: F401
+
+if TYPE_CHECKING:
+    # Nested type guard
+    from typing import Literal  # noqa: F401
 
 
 class AnotherClass:
